39f4fe6e66141abe7ad9e0ca2ab99f42c128c2b8
soumyabrataKarmakar <95677684+soumyabrataKarmakar@users.noreply.github.com>
2024-05-01 17:50:25 +0530

    bulk file upload endpoint implemented

--- src/categories/entities/get-category-wise-questions.dto.ts
+++ src/categories/entities/get-category-wise-questions.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class GetCategoryWiseQuestionDto {
-  @ApiProperty({ example: 10 })
+  @ApiProperty({ example: "66322d60c26f670a58ee951d" })
   @IsString()
   category_id: string;
 }
